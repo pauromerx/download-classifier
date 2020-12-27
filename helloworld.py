f2f339bc870510055a8aab7f5ab9313c51f222a4
Adri <adr1mt@users.noreply.github.com>
2020-12-27 18:26:05 +0100

    He comentado los problemas

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,18 @@
-pau = "gay"
+import os
+import shutil
 
-if pau == "gay":
-    print("Pau eres un jodido julandron, pero eres un hermano")
+# Localizamos la carpeta a organizar
+download_folder = "E:/adrim/Documents/Code/Carpeta a organizar"
+
+# Creamos una lista que guarde el nombre y la ext de todos los archivos de la carpeta
+list = os.listdir(download_folder)
+print(list)
+
+# Para cada posición de la lista separamos nombre y extensión
+# PROBLEMA: Si hay un punto intermedio en el nombre?
+# PROBLEMA: Hay que poner name y ext como listas
+"""
+for file in list:
+    name, ext = file.split(".")
+print(name, ext)
+"""
